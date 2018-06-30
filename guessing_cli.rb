f8a8f19fba721d1c8abce3ed6c9b04bf18839fde
Jesse McReady <jessemcready@gmail.com>
2018-06-30 03:42:53 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  secret_number = rand(1..6)
+  secret_number = rand(1..6).to_
   input = ""
   puts "Guess a number between 1 and 6."
   while input != "exit"
